8a7236fbcde77eff9a39677b074e22139c949d93
AlessandroIacovella95 <alessandroiacovella95@gmail.com>
2023-08-29 15:29:32 +0200

    add bonus

--- js/script.js
+++ js/script.js
@@ -3,7 +3,8 @@ const { createApp } = Vue;
 createApp({
   data() {
     return {
-      message: 'Hello world !'
+      message: 'Hello world !',
+      src:'./img/1.webp'
     }
   }
 }).mount('#app')
